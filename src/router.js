c3c01ee985bc5186b481ab9cf1b417ee942d37a4
Cushnie <cushniecolle@seattleu.edu>
2019-08-03 10:47:53 -0700

    changes to router.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from './components/HelloWorld'
-import Blog from './components/Blog'
+import HelloWorld from '@/components/HelloWorld'
+import Blog from '@/components/Blog'
 
 Vue.use(Router)
 
