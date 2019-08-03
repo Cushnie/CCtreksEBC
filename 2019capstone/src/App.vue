74b5589e13e46a9a0c7f4109472c00670ff89598
Cushnie <cushniecolle@seattleu.edu>
2019-08-03 08:31:30 -0700

    initial push, starting over

---
+++ 2019capstone/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="EBC" src="./assets/EBCGroup.jpg" width="640" height="420">
+    <HelloWorld msg="Namaste!"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
