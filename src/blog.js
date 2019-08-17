780a03d8a1f305ee9edc944a35aa9966e7e77a91
Cushnie <cushniecolle@seattleu.edu>
2019-08-17 08:06:31 -0700

    add Post.vue

--- src/blog.js
+++ src/blog.js
@@ -32,92 +32,92 @@ export default [
     },
     {
         'title': 'Day 5: The long slog up',
-        'to_city': 'Phakding',
-        'to_id': '1282758',
+        'from_city': 'Phakding',
+        'from_id': '1282758',
         'to_city': 'Namche Bazaar',
         'to_id': '1283001'
     },
     {
         'title': 'Day 6: We rest & find our breath',
-        'to_city': 'Namche Bazaar',
-        'to_id': '1283001',
+        'from_city': 'Namche Bazaar',
+        'from_id': '1283001',
         'to_city': 'Namche Bazaar',
         'to_id': '1283001'
     },
     {
         'title': 'Day 7: Have Diamox, will trek',
-        'to_city': 'Namche Bazaar',
-        'to_id': '1283001',
+        'from_city': 'Namche Bazaar',
+        'from_id': '1283001',
         'to_city': 'Dingboche',
         'to_id': '1283437'
     },
     {
         'title': 'Day 8: Himalayan sunsets',
-        'to_city': 'Dingboche',
-        'to_id': '1283437',
+        'from_city': 'Dingboche',
+        'from_id': '1283437',
         'to_city': 'Tengboche',
         'to_id': '1282670'
     },
     {
         'title': 'Day 9: Of course it snows',
-        'to_city': 'Tengboche',
-        'to_id': '1282670',
+        'from_city': 'Tengboche',
+        'from_id': '1282670',
         'to_city': 'Lobuche',
         'to_id': '1283011'
     },
     {
         'title': 'Day 10: EBC!!!',
-        'to_city': 'Lobuche',
-        'to_id': '1283011',
+        'from_city': 'Lobuche',
+        'from_id': '1283011',
         'to_city': 'Gorak Shep',
         'to_id': '1282670'
     },
     {
         'title': 'Day 11: The first long day down',
-        'to_city': 'Gorak Shep',
-        'to_id': '1282670',
+        'from_city': 'Gorak Shep',
+        'from_id': '1282670',
         'to_city': 'Pheriche',
         'to_id': '1282758'
     },
     {
         'title': 'Day 12: Are we there yet?',
-        'to_city': 'Pheriche',
-        'to_id': '1282758',
+        'from_city': 'Pheriche',
+        'from_id': '1282758',
         'to_city': 'Namche Bazaar',
         'to_id': '1283001'
     },
     {
         'title': 'Day 13: What goes down, must go up',
-        'to_city': 'Namche Bazaar',
-        'to_id': '1283001',
+        'from_city': 'Namche Bazaar',
+        'from_id': '1283001',
         'to_city': 'Lukla',
         'to_id': '1282758'
     },
     {
         'title': 'Day 14: Waiting in airports',
-        'to_city': 'Lukla',
-        'to_id': '1282758',
+        'from_city': 'Lukla',
+        'from_id': '1282758',
         'to_city': 'Kathmandu',
         'to_id': '1283240'
     },
     {
         'title': 'Day 15: Spa day & cultural night',
-        'to_city': 'Kathmandu',
-        'to_id': '1283240',
+        'from_city': 'Kathmandu',
+        'from_id': '1283240',
         'to_city': 'Kathmandu',
         'to_id': '1283240'
     },
     {
         'title': 'Day 16: Happy Birthday to me',
-        'to_city': 'Kathmandu',
-        'to_id': '1283240',
+        'from_city': 'Kathmandu',
+        'from_id': '1283240',
         'to_city': 'Guangzhou',
         'to_id': '1809858'
     },
     {
         'title': 'Day 17: Back in the USA',
-        'to_city': 'Vancouver',
-        'to_id': '6173331',
+        'from_city': 'Vancouver',
+        'from_id': '6173331',
         'to_city': 'Seattle',
         'to_id': '5809844'
     },
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -3,43 +3,72 @@
     <div class="text">Namaste</div>
 
     <div class="cities"></div>
+    <div v-if="blog">
+      <!-- <weather :id="blog.from_id"></weather> -->
+      <!-- <weather :id="blog.to_id"></weather> -->
+    </div>
   </div>
 </template>
 
 
 <script>
 import axios from "axios";
+import Weather from "@/components/Weather";
 
 export default {
   name: "Post",
-  data () {
+  components: {
+    weather: Weather
+  },
+
+  data() {
     return {
       posts: [],
       errors: [],
       blog: null,
-      weather: null
+      weather: null,
+      fromId: "",
+      toId: ""
+
     };
   },
-  created () {
-    this.blog = this.$route.params.blog
-    console.log(this.blog)
+  created() {
+    this.blog = this.$route.params.blog;
+    console.log(this.blog);
     //get first weather
-    let fromId = this.blog.from_id
-    console.log(fromId)
+    this.fromId = this.blog.from_id;
+    console.log(this.fromId);
+    this.toId = this.blog.to_id;
+    console.log(this.toId)
 
-        axios
-      .get('//api.openweathermap.org/data/2.5/weather?id=2172797', {        params: {
-          id: this.blog.from_id,
-          APPID: '883942609bd04e6ecf2a33c7ab270cbb'
-        }
-      })
-      .then(response => {
-        this.weather = response.data;
-        console.log(this.weather)
-      })
-      .catch(e => {
-        this.errors.push(e);
-      });
+    // axios
+    //   .get("//api.openweathermap.org/data/2.5/weather", {
+    //     params: {
+    //       id: this.blog.from_id,
+    //       APPID: "883942609bd04e6ecf2a33c7ab270cbb"
+    //     }
+    //   })
+    //   .then(response => {
+    //     this.weather = response.data;
+    //     console.log(this.weather);
+    //   })
+    //   .catch(e => {
+    //     this.errors.push(e);
+    //   });
+    // axios
+    //   .get("//api.openweathermap.org/data/2.5/weather", {
+    //     params: {
+    //       id: this.blog.to_id,
+    //       APPID: "883942609bd04e6ecf2a33c7ab270cbb"
+    //     }
+    //   })
+    //   .then(response => {
+    //     this.weather = response.data;
+    //     console.log(this.weather);
+    //   })
+    //   .catch(e => {
+    //     this.errors.push(e);
+    //   });
   }
 };
 </script>
