a7d10b607e4f0ff6f209d6cb9c66b62c2f857241
Cushnie <cushniecolle@seattleu.edu>
2019-08-25 18:56:22 -0700

    added alt tags & captions to my pictures

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -3,235 +3,235 @@
   
     <ul>
       <li>
-        <img src="../assets/IMG7760.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7760.jpg" alt="boots&pack"/>
+        <br />All geared up & ready to go
       </li>
      
       <li>
-        <img src="../assets/IMG7776.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7776.jpg" alt="YVRsculpture"/>
+        <br />I love the Haida Gawaii art here at YVR
       </li>
       <li>
-        <img src="../assets/IMG7779.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7779.jpg" alt="sleepinginYVR"/>
+        <br />Trying to sleep in an aiport. 
       </li>
       <li>
-        <img src="../assets/IMG7780.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7780.jpg" alt="TimHortons "/>
+        <br />When in Canada ...
       </li>
       <li>
-        <img src="../assets/IMG7786.jpg" />
-        <br />caption here
-      </li>
-
-      <li>
-        <img src="../assets/IMG7803.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7805.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7808.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7809.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7815.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7823.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7824.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7825.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7829.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7835.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7840.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7844.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7786.jpg" alt="seastars"/>
+        <br />I was hoping to crash out here & not in the ticketing area
       </li>
 
       <li>
-        <img src="../assets/IMG7852.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7803.jpg" alt="CANairport"/>
+        <br />Exit of Flight Cancellations? I'm thinking Google Translate failed someone somewhere here.
       </li>
       <li>
-        <img src="../assets/IMG7859.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7805.jpg" alt="KTM"/>
+        <br />Over 24 hours later, I arrive in Kathmandu.
       </li>
       <li>
-        <img src="../assets/IMG7863.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7808.jpg" alt="Namaste"/>
+        <br />So tired yet so excited & in love with my marigold welcome lei
       </li>
       <li>
-        <img src="../assets/IMG7864.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7809.jpg" alt="Thimel"/>
+        <br />Exploring the sights of Thimel
       </li>
       <li>
-        <img src="../assets/IMG7866.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7815.jpg" alt="wires"/>
+        <br />I wonder which wire does what ... 
       </li>
       <li>
-        <img src="../assets/IMG7868.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7823.jpg" alt="beer"/>
+        <br />Mmm ... Everest beer 
       </li>
       <li>
-        <img src="../assets/IMG7871.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7824.jpg" alt="door"/>
+        <br />Exploring the Pashupatinath Temple, a UNESCO World Heritage Site
       </li>
       <li>
-        <img src="../assets/IMG7885.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7825.jpg" alt="colors"/>
+        <br />I love the vibrant colors of this country.
       </li>
       <li>
-        <img src="../assets/IMG7887.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7829.jpg" alt="blessed"/>
+        <br />Receiving a blessing to wish us a good trek and favorable conditions
       </li>
       <li>
-        <img src="../assets/IMG7895.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7835.jpg" alt="pagoda"/>
+        <br />Did you know that Nepal is home to the original pagoda?
       </li>
       <li>
-        <img src="../assets/IMG7899.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7840.jpg" alt="gate"/>
+        <br />The beautiful gate to the Boudhanath stupa, another UNESCO World Heritage Site
       </li>
       <li>
-        <img src="../assets/IMG7905.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7908.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7911.jpg" />
-        <br />caption here
-      </li>
-      <li>
-        <img src="../assets/IMG7919.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7844.jpg" alt="eyes"/>
+        <br />This was heavily damaged in the 2015 earthquake however it was restored thanks in part to the Tibetan refugees who revere this site as sacred and holy
       </li>
 
       <li>
-        <img src="../assets/IMG7926.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7852.jpg" alt="flags"/>
+        <br />The prayer flags of Buddhism
       </li>
       <li>
-        <img src="../assets/IMG7948.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7859.jpg" alt="pass"/>
+        <br />And off to the Himalayas we go!
       </li>
       <li>
-        <img src="../assets/IMG7955.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7863.jpg" alt="plane1"/>
+        <br />We're on a very small plane.
       </li>
       <li>
-        <img src="../assets/IMG7958.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7864.jpg" alt="plane2"/>
+        <br />Whoa. Is that the cockpit?!? 
       </li>
       <li>
-        <img src="../assets/IMG7962.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7866.jpg" alt="prop"/>
+        <br />Yes the plane is a little twin prop job. Not for the faint of heart.
       </li>
       <li>
-        <img src="../assets/IMG7973.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7868.jpg" alt="begin"/>
+        <br />We survived the world's most dangerous airport & got a quick safety briefing so off we go
       </li>
       <li>
-        <img src="../assets/IMG7977.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7871.jpg" alt="me1"/>
+        <br />In a state of disbelief that this is really happening. That's the Dudh Kosi river. The water comes from the glaciers of Everest. 
       </li>
       <li>
-        <img src="../assets/IMG7987.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7885.jpg" alt="handle"/>
+        <br />Even the bathroom door handles are pretty here.
+      </li>
+      <li>
+        <img src="../assets/IMG7887.jpg" alt="view1"/>
+        <br />The morning view from my room. Wow!
+      </li>
+      <li>
+        <img src="../assets/IMG7895.jpg" alt="view2"/>
+        <br />We stopped here for a little tea & rest. The views do not disappoint.
+      </li>
+      <li>
+        <img src="../assets/IMG7899.jpg" alt="park"/>
+        <br />Stopping to check into the park. Everyone has to show their passport & proof that they paid to be there. Also, you're not allowed in without a guide. 
+      </li>
+      <li>
+        <img src="../assets/IMG7905.jpg" alt="bridge1"/>
+        <br />Yep. We're going across that. No, not the lower one. The higher & longer one. If you have a fear of heights, this one will scare you. 
+      </li>
+      <li>
+        <img src="../assets/IMG7908.jpg" alt="bridge2"/>
+        <br />And. Here. We. Go!
+      </li>
+      <li>
+        <img src="../assets/IMG7911.jpg" alt="bridge3"/>
+        <br />Holy cow that was intense! Oh and did I mention you can't be on the bridge at the same time when animals are crossing? There was a donkey train behind us so we had to pick up the pace a little. 
+      </li>
+      <li>
+        <img src="../assets/IMG7919.jpg" alt="NB1"/>
+        <br />I finally made it to Namche Bazaar and believe I experienced all four seasons in the last 8-10 hours. I'm now at 11,500 feet above sea level.
       </li>
 
       <li>
-        <img src="../assets/IMG7999.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7926.jpg" alt="NB2"/>
+        <br />Rest day in Namche Bazaar and our first little peek at Everest herself. Here she's being shy but you can still see her. Started the diamox today as last night I felt the first effects of altitude.
       </li>
       <li>
-        <img src="../assets/IMG8003.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7948.jpg" alt="bar2"/>
+        <br />The highest bar in the world and of course it's an Irish one. 
       </li>
       <li>
-        <img src="../assets/IMG8014.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7955.jpg" alt="view2"/>
+        <br />These mountains and clouds are so beautiful. Words are impossible here.
       </li>
       <li>
-        <img src="../assets/IMG8016.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7958.jpg" alt="flags"/>
+        <br />The weather can change on you quickly & without warning so you must be prepared for anything at all times.
       </li>
       <li>
-        <img src="../assets/IMG8022.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7962.jpg" alt="me2"/>
+        <br />Taking a moment to pause & reflect on that I'm really here in the Nepalese Himalayas seeing some of the highest mountains in the world. 
       </li>
       <li>
-        <img src="../assets/IMG8027.jpg" />
-        <br />caption here
+        <img src="../assets/IMG7973.jpg" alt="gate2"/>
+        <br />In Tengboche and the monastery is the highest in the world. John & Chris Berlow getting a brothers selfie for the wives back home.
+      </li>
+      <li>
+        <img src="../assets/IMG7977.jpg" alt="views3"/>
+        <br />The evening in Tengboche did not disappoint. But as we climb higher, we get further & further away from the tree line. Soon it will be nothing but rocks & dirt.
+      </li>
+      <li>
+        <img src="../assets/IMG7987.jpg" alt="views4"/>
+        <br />Good morning, Mt. Everest! That's the jet stream blowing snow off her peak. 
+      </li>
+
+      <li>
+        <img src="../assets/IMG7999.jpg" alt="me4"/>
+        <br />Definitely above the tree line now. And still more miles to go.
+      </li>
+      <li>
+        <img src="../assets/IMG8003.jpg" alt="views5"/>
+        <br />I can't get enough of how beautiful it is up here.
+      </li>
+      <li>
+        <img src="../assets/IMG8014.jpg" alt="me5"/>
+        <br />On our way from Lobuche to EBC! 
+      </li>
+      <li>
+        <img src="../assets/IMG8016.jpg" alt="pants"/>
+        <br />Wearing the fancy pants for a momentous occasion.
+      </li>
+      <li>
+        <img src="../assets/IMG8022.jpg" alt="views6"/>
+        <br />Walking along the glacier. Everest is the peak in the distance.
+      </li>
+      <li>
+        <img src="../assets/IMG8027.jpg" alt="EBC1"/>
+        <br />I DID IT!!! 17,600 feet above sea level! 
       </li>
       <!-- <li>
         <img src="../assets/IMG8028.jpg" />
         <br />caption here
       </li> -->
       <li>
-        <img src="../assets/IMG8042.jpg" />
-        <br />caption here
+        <img src="../assets/IMG8042.jpg" alt="descent1"/>
+        <br />Now begins the long trek back and into lower elevations and more oxygen. I can already feel my lungs working better and my body absorbing all the oxygen it's accumulating.
       </li>
 
       <li>
-        <img src="../assets/IMG8048.jpg" />
-        <br />caption here
+        <img src="../assets/IMG8048.jpg" alt="descent2"/>
+        <br />Pheriche to Namche Bazaar. You can tell we're descending because LOOK! Green plants!!!
       </li>
       <li>
-        <img src="../assets/IMG8055.jpg" />
-        <br />caption here
+        <img src="../assets/IMG8055.jpg" alt="descent3"/>
+        <br />Our last day of trekking from Namche Bazaar to Lukly and Everest offers us one last lovely view of her majestic presence
       </li>
       <li>
-        <img src="../assets/IMG8061.jpg" />
-        <br />caption here
+        <img src="../assets/IMG8061.jpg" alt="lukla"/>
+        <br />And we're under 10,000 feet elevation. Hi Lukla. 
       </li>
       <li>
-        <img src="../assets/IMG8062.jpg" />
-        <br />caption here
+        <img src="../assets/IMG8062.jpg" alt="flag"/>
+        <br />Having a celebratory beer at the local pub in Lukla. I loved this flag & signed my name on the wall. 
       </li>
 
       <li>
-        <img src="../assets/IMG8069.jpg" />
-        <br />caption here
+        <img src="../assets/IMG8069.jpg" alt="gorp"/>
+        <br />Time for shenanigans, Gorp the Yeti, and really bad whisky. 
       </li>
       <li>
-        <img src="../assets/IMG8096.jpg" />
-        <br />caption here
+        <img src="../assets/IMG8096.jpg" alt="meKTM"/>
+        <br />It's my birthday and time to leave Nepal. I'm sad to leave but also super proud of the achievement I accomplished. Already thinking about my trip back to Nepal.
       </li>
       <li>
-        <img src="../assets/IMG8097.jpg" />
-        <br />caption here
+        <img src="../assets/IMG8097.jpg" alt="gear"/>
+        <br />Allll the gear & bags
       </li>
       <li>
-        <img src="../assets/IMGEBCGroup.jpg" />
-        <br />caption here
+        <img src="../assets/IMGEBCGroup.jpg" alt="Climb2Cure"/>
+        <br />The members of the Climb2Cure team! Together we raised over $125,000 for the Leukemia & Lymphoma Society in their quest to erradicate blood cancers. GO TEAM! 
       </li>
     </ul>
 
